5fc7245ce7c8b325e927f5630d25e7c77a6fdbe0
Candice Tourrette <candicetourrette@MacBook-Air-de-Candice.local>
2023-06-14 14:08:16 +0200

    bug pop up

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
-$(".count").each(function () {
-    $(this)
-      .prop("Counter", 0)
-      .animate(
-        {
-          Counter: $(this).text()
-        },
-        {
-          duration: 4000,
-          easing: "swing",
-          step: function (now) {
-            $(this).text(Math.ceil(now));
-          }
-        }
-      );
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'linear',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
   });
-  
+});
 
 
+function openPopup() {
+  var popup = document.querySelector('.popup-container');
+  popup.classList.add('active');
+}
 
-
+function closePopup() {
+  var popup = document.querySelector('.popup-container');
+  popup.classList.remove('active');
+}
