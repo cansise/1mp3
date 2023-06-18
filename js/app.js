6c1a9ff02e3ca0554f58582bb79b1a67b9b210df
Candice Tourrette <candicetourrette@MacBook-Air-de-Candice.local>
2023-06-18 14:09:13 +0200

    version sans reponsive mais desktop sans bug

--- js/app.js
+++ js/app.js
@@ -11,12 +11,36 @@ $('.count').each(function () {
 });
 
 
-function openPopup() {
-  var popup = document.querySelector('.popup-container');
-  popup.classList.add('active');
+function openPopupalan() {
+  var popup1 = document.getElementById('popup-alan');
+  popup1.classList.add('active');
 }
 
-function closePopup() {
-  var popup = document.querySelector('.popup-container');
-  popup.classList.remove('active');
+function closePopupalan() {
+  var popup2 = document.getElementById('popup-alan');
+  popup2.classList.remove('active');
+}
+
+
+
+function openPopupalice() {
+  var popup3 = document.getElementById('popup-alice');
+  popup3.classList.add('active');
+}
+
+function closePopupalice() {
+  var popup4 = document.getElementById('popup-alice');
+  popup4.classList.remove('active');
+}
+
+
+
+function openPopupsource() {
+  var popup3 = document.getElementById('popup-source');
+  popup3.classList.add('active');
+}
+
+function closePopupsource() {
+  var popup4 = document.getElementById('popup-source');
+  popup4.classList.remove('active');
 }
--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ const labels = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
 const data = {
   labels: labels,
   datasets: [{
-    label: 'My First Dataset',
+    label: 'Nombre de recheches /1000 du mot : cyberharcelement',
     data: [0.75, 1.8, 2, 3.25, 4, 5.16, 5.66, 5.75, 6.25, 8.5, 8.58, 12.66, 31.68],
     fill: false,
     borderColor: 'rgb(250, 0, 127)',
